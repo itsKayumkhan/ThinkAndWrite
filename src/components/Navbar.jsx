c807fb59e48a9470b3deec82afe3b91625c223a7
kayumkhan_sayal <kayumkhankks45@gmail.com>
2023-10-20 20:32:28 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
+        <div className="container">
+          <NavLink className="navbar-brand logo" to="/">
+            <i
+              className="fa-solid fa-store pe-3"
+              style={{ color: "#080808" }}
+            ></i>
+            E-commerce
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="/navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contact">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
